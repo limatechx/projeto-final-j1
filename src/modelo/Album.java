e957878b6562782d215a498ec5847835e4b34d3b
Carlos Eduardo Lima <carloseduardolima.dev@gmail.com>
2025-05-31 19:37:25 -0300

    feat: Finalizado configuração inicial dos modelos

--- src/modelo/Album.java
+++ src/modelo/Album.java
@@ -1,4 +1,24 @@
 package modelo;
 
 public class Album extends Item {
+    private String artista;
+    private int qtdDeFaixas;
+
+    public Album(String nome, String categoria, String artista, int qtdDeFaixas) {
+        super(nome, categoria);
+        this.artista = artista;
+        this.qtdDeFaixas = qtdDeFaixas;
+    }
+    public String getArtista() {
+        return artista;
+    }
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+    public int getQtdDeFaixas() {
+        return qtdDeFaixas;
+    }
+    public void setQtdDeFaixas(int qtdDeFaixas) {
+        this.qtdDeFaixas = qtdDeFaixas;
+    }
 }
--- src/modelo/Revista.java
+++ src/modelo/Revista.java
@@ -1,4 +1,24 @@
 package modelo;
 
 public class Revista extends Item{
+    private String editora;
+    private int edicao;
+
+    public Revista(String nome, String categoria, String editora, int edicao) {
+        super(nome, categoria);
+        this.editora = editora;
+        this.edicao = edicao;
+    }
+    public String getEditora() {
+        return editora;
+    }
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+    public int getEdicao() {
+        return edicao;
+    }
+    public void setEdicao(int edicao) {
+        this.edicao = edicao;
+    }
 }
