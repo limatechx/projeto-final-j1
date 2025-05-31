cd4be555fbf7a4df2306ba5673e4794d43213928
Carlos Eduardo Lima <carloseduardolima.dev@gmail.com>
2025-05-31 20:19:22 -0300

    feat: configuração do Bando de dados

--- src/repositorio/BancoDeDados.java
+++ src/repositorio/BancoDeDados.java
@@ -1,4 +1,39 @@
 package repositorio;
 
+import modelo.Item;
+
+import java.util.*;
+
 public class BancoDeDados {
+    private static List<Item> itens = new ArrayList<>();
+
+    public static void adicionarItem(Item item) {
+        itens.add(item);
+    }
+
+    public static void removerItem(Item item) {
+        itens.remove(item);
+    }
+
+    public static void atualizarItem(String itemAntigo, Item novoItem) {
+        Item item = localizarItem(itemAntigo);
+        if (item != null) {
+            itens.set(itens.indexOf(item), novoItem);
+        }
+    }
+
+    public static void listarItens() {
+        for (Item item : itens) {
+            System.out.println(item);
+        }
+    }
+
+    public static Item localizarItem(String nome) {
+        for (Item item : itens) {
+            if (item.getNome().equalsIgnoreCase(nome)) {
+                return item;
+            }
+        }
+        return null;
+    }
 }
